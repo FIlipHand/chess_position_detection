b6ba90bc418670676b0b56b9aa15a2b4cb797249
GoldenHandyMan <33002819+GoldenHandyMan@users.noreply.github.com>
2021-07-07 18:51:57 +0200

    now returns fen and not dict

--- src/models/model.py
+++ src/models/model.py
@@ -10,9 +10,25 @@ FEN_dict = {'black_bishop': 'b', 'black_king': 'k', 'black_knight': 'n', 'black_
 model = load_model('C:\\Users\\filif\\Desktop\\programowanko\\chess_position_detection\\src\\models\\acc1.h5')
 
 
-def get_model_predictions(encoded_image) -> dict:
+def get_model_predictions(encoded_image) -> str:
     images = get_figures_arrays(encoded_image, True)
-    preds = {}
-    for i, img in enumerate(images):
-        preds[i] = dict(zip(list(FEN_dict.keys()), [str(x) for x in list(model.predict(img).reshape(-1))]))
-    return preds
+    i = 1
+    space = 0
+
+    notation = ''
+    for img in images:
+        if is_figure(img):
+            prediction = model.predict(img)
+            if space is not 0:
+                notation += str(space)
+                space = 0
+            notation += (FEN_dict[list(FEN_dict.keys())[np.argmax(prediction)]])
+        else:
+            space += 1
+        if i % 8 is 0:
+            if space is not 0:
+                notation += str(space)
+            notation += '/'
+            space = 0
+        i += 1
+    return notation
